69cf2ed09697c4b71d9b61b9a2a38ad138093c2c
Рузиева Флора <fvaka84@gmail.com>
2025-05-11 22:33:12 +0300

    Добавлен новый класс с названием Equipment

---
+++ Управление техникой/Управление техникой/Equipment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+public class Equipment
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string SerialNumber { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public DateTime LastMaintenanceDate { get; set; }
+    public EquipmentStatus Status { get; set; }
+    public Equipment(string name, string type, string serialNumber, DateTime purchaseDate,
+    DateTime lastMaintenanceDate, EquipmentStatus status)
+    {
+        Name = name;
+        Type = type;
+        SerialNumber = serialNumber;
+        PurchaseDate = purchaseDate;
+        LastMaintenanceDate = lastMaintenanceDate;
+        Status = status;
+    }
+    public override string ToString()
+    {
+        return $"Оборудование: {Name}\nТип: {Type}\nСерийный номер: {SerialNumber}\n"
+        +
+        $"Дата покупки: {PurchaseDate.ToString("dd.MM.yyyy")}\nДата последнего обслуживания: { LastMaintenanceDate.ToString("dd.MM.yyyy")}\nСостояние: { Status}";
+}
+    public void PerformMaintenance()
+    {
+        LastMaintenanceDate = DateTime.Now;
+        Status = EquipmentStatus.InGoodCondition;
+        MessageBox.Show($"Обслуживание оборудования '{Name}' выполнено.");
+    }
+    public void MarkAsBroken()
+    {
+        Status = EquipmentStatus.Broken;
+        MessageBox.Show($"Оборудование '{Name}' помечено как неисправное.");
+    }
+    public void MarkAsInRepair()
+    {
+        Status = EquipmentStatus.InRepair;
+        MessageBox.Show($"Оборудование '{Name}' отправлено в ремонт.");
+    }
+}
+public enum EquipmentStatus
+{
+    InGoodCondition,
+    Broken,
+    InRepair
+}
