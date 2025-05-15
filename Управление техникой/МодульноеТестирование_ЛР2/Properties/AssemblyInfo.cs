9ef57f3988a29dcff60e1e8d419803c26cd69b3d
Рузиева Флора <fvaka84@gmail.com>
2025-05-15 23:02:57 +0300

    Добален проект для модульных тестов - МодульноеТестирование

---
+++ Управление техникой/МодульноеТестирование_ЛР2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("МодульноеТестирование_ЛР2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("МодульноеТестирование_ЛР2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d029e49a-adb3-42b8-8582-7d1b2685007b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
