47cf01e13673ea008fe4d80bb152828a4a054c51
unknown <3291922-30>
2025-05-16 11:42:10 +0300

    Добавлены новые тестовые модули в проект Модульного тестирования

--- Управление техникой/МодульноеТестирование_ЛР2/UnitTest1.cs
+++ Управление техникой/МодульноеТестирование_ЛР2/UnitTest1.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Управление_техникой;
 
 namespace МодульноеТестирование_ЛР2
@@ -51,7 +51,21 @@ namespace МодульноеТестирование_ЛР2
         }
 
         [TestMethod]
-        public void PerformMaintenance_SetsCurrentDateAndGoodStatus()
+        public void PerformMaintenance_SetsCurrentDate()
+        {
+            // Arrange
+            var equipment = new Equipment("Printer", "Office", "A00001", new DateTime(2024, 05, 10), new DateTime(2025, 05, 10), EquipmentStatus.Broken);
+            var beforeTest = DateTime.Now;
+
+            // Act
+            equipment.PerformMaintenance();
+
+            // Assert
+            Assert.IsTrue(equipment.LastMaintenanceDate >= beforeTest);
+        }
+
+        [TestMethod]
+        public void PerformMaintenance_SetsCurrentGoodStatus()
         {
             // Arrange
             var equipment = new Equipment("Printer", "Office", "A00001", new DateTime(2024, 05, 10), new DateTime(2025, 05, 10), EquipmentStatus.Broken);
@@ -62,7 +76,20 @@ namespace МодульноеТестирование_ЛР2
 
             // Assert
             Assert.AreEqual(EquipmentStatus.InGoodCondition, equipment.Status);
-            Assert.IsTrue(equipment.LastMaintenanceDate >= beforeTest);
+        }
+
+        [TestMethod]
+        public void PerformMaintenance_ShowMessage()
+        {
+            // Arrange
+            var equipment = new Equipment("Printer", "Office", "A00001", new DateTime(2024, 05, 10), new DateTime(2025, 05, 10), EquipmentStatus.Broken);
+            var beforeTest = DateTime.Now;
+
+            // Act
+            equipment.PerformMaintenance();
+
+            // Assert
+            // проверить, появилось ли сообщение о том, что обслуживание заданного оборудования выполнено 
         }
 
         [TestMethod]
@@ -79,6 +106,19 @@ namespace МодульноеТестирование_ЛР2
         }
 
         [TestMethod]
+        public void MarkAsBroken_ShowMessage()
+        {
+            // Arrange
+            var equipment = new Equipment("Printer", "Office", "A00001", new DateTime(2024, 05, 05), new DateTime(2025, 05, 05), EquipmentStatus.InGoodCondition);
+
+            // Act
+            equipment.MarkAsBroken();
+
+            // Assert
+            // проверить, появилось ли сообщение о том, что заданное оборудование помечено как неисправное 
+        }
+
+        [TestMethod]
         public void MarkAsInRepair_SetsInRepairStatus()
         {
             // Arrange
@@ -90,6 +130,19 @@ namespace МодульноеТестирование_ЛР2
             // Assert
             Assert.AreEqual(EquipmentStatus.InRepair, equipment.Status);
         }
+
+        [TestMethod]
+        public void MarkAsInRepair_ShowMessage()
+        {
+            // Arrange
+            var equipment = new Equipment("Printer", "Office", "A00001", new DateTime(2024, 04, 20), new DateTime(2025, 04, 20), EquipmentStatus.Broken);
+
+            // Act
+            equipment.MarkAsInRepair();
+
+            // Assert
+            // проверить, появилось ли сообщение о том, что заданное оборудование отправлено на ремонт 
+        }
     }
 
     // тесты для класса EquipmentManager
@@ -97,7 +150,9 @@ namespace МодульноеТестирование_ЛР2
     public class EquipmentManagerTests
     {
         private EquipmentManager _manager;
+        private EquipmentManager equipmentManager;
         private ListView _listView;
+        private ListView listView;
 
         [TestInitialize]
         public void Setup()
@@ -131,6 +186,27 @@ namespace МодульноеТестирование_ЛР2
         }
 
         [TestMethod]
+        public void AddEquipment_WithNull_ShowMessage()
+        {
+            // Act
+            _manager.AddEquipment(null);
+
+            // Assert
+            // проверить, появилось ли сообщение о том, что объект оборудования не создан
+        }
+
+        [TestMethod]
+        public void RemoveEquipment_WithExistingSerialNumber_FindsEquipment()
+        {
+            // Arrange
+            var equipment = new Equipment("Printer", "Office", "A00001", DateTime.Now, DateTime.Now, EquipmentStatus.InGoodCondition);
+            _manager.AddEquipment(equipment);
+
+            // Assert
+            StringAssert.Contains(equipment.SerialNumber, "A00001");
+        }
+
+        [TestMethod]
         public void RemoveEquipment_WithExistingSerialNumber_RemovesFromList()
         {
             // Arrange
@@ -159,13 +235,68 @@ namespace МодульноеТестирование_ЛР2
         }
 
         [TestMethod]
+        public void RemoveEquipment_WithNonExistingSerialNumber_ShowMessage()
+        {
+            // Arrange
+            var equipment = new Equipment("Printer", "Office", "A00001", DateTime.Now, DateTime.Now, EquipmentStatus.InGoodCondition);
+            _manager.AddEquipment(equipment);
+
+            // Act
+            _manager.RemoveEquipment("0000");
+
+            // Assert
+            // проверить, появилось ли сообщение о том, что объект оборудования с таким серийным нномером не найден
+        }
+
+        [TestMethod]
+        public void DisplayEquipmentInfo_EmptyMessage()
+        {
+            _manager.DisplayEquipmentInfo();
+
+            // Assert
+            // проверить, что появилось сообщение о пустом списке
+        }
+
+        [TestMethod]
         public void LoadEquipment_WithEmptyList_ShowsEmptyListView()
         {
-            // Act (LoadEquipment is called internally after Add/Remove)
+            // Act вызывается внутри после Add/Remove
             // No equipment added
 
             // Assert
             Assert.AreEqual(0, _listView.Items.Count);
+        }
+    }
+
+    // тесты для формы MainForm
+    [TestClass]
+    public class MainFormTests
+    {
+        [TestMethod]
+        public void MainForm_FormDisplay()
+        {
+            // Act 
+            var form = new MainForm();
+
+            // Assert
+            // проверить, что появлилась форма 
+        }
+    }
+
+
+    // тесты для класса DisplayInfoForm
+    [TestClass]
+    public class DisplayInfoFormTests
+    {
+        [TestMethod]
+        public void DisplayInfoForm_FormDisplayEmptyMessage()
+        {
+            // Act
+            var form = new MainForm();
+            form.ShowDialog();
+
+            // Assert
+            // проверить, что вышло сообщение о том что список оборудования пуст или не задан 
         }
     }
 }
